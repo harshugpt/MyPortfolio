03829299a073f3ce4ecf38551a0766a52122cf85
harshugpt <uharshgpt2912@gmail.com>
2023-10-28 19:00:20 +0530

    added resume and demo email form

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 $(document).ready(function () {
+  function isValidEmail(email) {
+    // Regular expression for basic email validation
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  }
+
+  // Form submission event
+  $("#submit").click(function (e) {
+    e.preventDefault(); // Prevent the form from submitting
+
+    // Get user inputs
+    const name = $("#name").val();
+    const email = $("#email").val();
+    const subject = $("#subject").val();
+    const message = $("#message").val();
+
+    // Validate email
+    if (!isValidEmail(email)) {
+      alert("Please enter a valid email address.");
+      return; // Stop form submission
+    }
+
+    // Additional validation can be added here if needed
+
+    // If all validations pass, you can proceed with form submission
+    // You can use AJAX to submit the form data to your server
+    // Example: $.post("your_server_url", { name, email, subject, message }, function(data) { ... });
+
+    // Clear form fields (or you can redirect to a thank you page)
+    $("#name").val("");
+    $("#email").val("");
+    $("#subject").val("");
+    $("#message").val("");
+
+    alert("Message sent successfully!...this is for demo");
+  });
+
   $(window).scroll(function () {
     if (this.scrollY > 20) {
       $(".navbar").addClass("sticky");
